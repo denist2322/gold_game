088697abf78c4e72e723dde68c9c8a68fdb78a04
denist2322 <parksomang125@gmail.com>
2022-06-15 23:17:22 +0900

    2.배경화면

--- gold_mine.py
+++ gold_mine.py
@@ -1,4 +1,5 @@
 # 기본 뼈대 생성
+import os
 import pygame
 
 pygame.init()
@@ -10,6 +11,8 @@ pygame.display.set_caption("Gold Miner")
 
 clock = pygame.time.Clock()
 
+current_path = os.path.dirname(__file__)  # 현재 파일의 위치 반환
+background = pygame.image.load(os.path.join(current_path, "background.png"))
 
 running = True
 # 게임실행
@@ -19,5 +22,8 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    screen.blit(background, (0, 0))
+    pygame.display.update()
 
 pygame.quit()
